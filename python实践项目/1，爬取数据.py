7bd84f31ab83e06c3d8613915149e324a5a67ba4
刘栋 <chuangye201012@163.com>
2020-03-03 11:59:07 +0800

    Create 1，爬取数据.py

---
+++ python实践项目/1，爬取数据.py
@@ -0,0 +1,12 @@
+# 数据挖掘：urllib库（python自带的库）不需安装，request库（第三方库）需要下载安装
+# 数据清洗：python正则表达式，常用；beautifulsoup框架；scrapy框架
+import 
+"""a = "hello"
+turble_a = 1,2
+turble_b = {1,2}
+print(turble_a == turbel_b)
+print(a[0])
+a = {"key":1,"mother":2}
+print("key" in a)
+print(a.get("key"))"""
+population = {"shanghai":17.8,"litanbul":13.3,"karachi":13.0,"mumbai":12.5}
